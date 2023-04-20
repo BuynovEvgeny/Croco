41acea9a348d9c1a941b51e78255bd1e8fa491a5
BuynovEvgeny <buynov.evgeny@gmail.com>
2023-04-20 23:34:47 +0200

    Update horizontal.blade.php
    убрал кнопку соцсети google+

--- horizontal.blade.php
+++ horizontal.blade.php
@@ -1,8 +1,10 @@
+<!--  C:\OSPanel\domains\croco.org\resources\views\layouts\inc\social\horizontal.blade.php
+убрал кнопку соцсети google+ -->
+
 <div class="text-center mt-4 mb-4 ms-0 me-0">
     <p>Поделитесь в соцсетях</p>
 	<button class='btn btn-fb share s_facebook'><i class="fab fa-facebook"></i> </button>&nbsp;
 	<button class='btn btn-fb share s_vk' type='button'>vk<span class='badge counter c_vk'></span></button>&nbsp;
 	<button class='btn btn-wa share s_whatsapp'><i class="fab fa-whatsapp"></i> </button>&nbsp;
 	<button class='btn btn-lkin share s_linkedin'><i class="fab fa-linkedin"></i> </button>&nbsp;
-	<button class='btn btn-fb share s_plus'><i class="bi bi-google"></i></button>
 </div>
